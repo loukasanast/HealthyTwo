a3b720cd00bc3feab718a6d2fe8bb76eeb248caf
loukasanast <me@anastasiou.email>
2016-03-05 22:24:41 +0100

    Init
    Initial Commit

---
+++ HealthyTwo/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib;
+
+namespace HealthyTwo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+            Repository.Test();
+        }
+
+        private void imgClose_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void imgMinimize_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
---
+++ Lib/Repository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class Repository
+    {
+        public static WebClient Client { get; set; }
+
+        public static void Test()
+        {
+            Client = new WebClient();
+            //Client.Headers.Add("Authorization: bearer GQVfuojjAOHWk9ESwwUnlTMs7CTM4ZKF");
+            //Client.Headers["Authorization"] = "bearer GQVfuojjAOHWk9ESwwUnlTMs7CTM4ZKF";
+            String result = Client.DownloadString("http://bing.com");
+            //Console.WriteLine(result);
+            //Console.Read();
+            Debug.WriteLine(result); 
+        }
+    }
+}
